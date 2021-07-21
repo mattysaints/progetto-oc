264f31961ee5da123d95e37b77dd56df3742bd48
Davide Zago <davide.zago@edu.unito.it>
2021-07-21 12:03:11 +0200

    Refactor e aggiunta test preso da Balas, Toth

--- efficiency_test.py
+++ efficiency_test.py
@@ -10,9 +10,9 @@ from tsp import TSP
 from tsp_parser import to_mathprog
 
 
-def get_13_cities(n):
+def get_13_cities_tsp(n):
     """Returns a submatrix nxn of the 13 cities instance"""
-    return np.triu([
+    return TSP(np.triu([
         [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
         [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
         [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
@@ -26,7 +26,21 @@ def get_13_cities(n):
         [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
         [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
         [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
-    ])[:n, :n]
+    ])[:n, :n])
+
+
+def get_balas_ex1_tsp():
+    """Example 1 of Balas, Toth [1983]"""
+    return TSP(np.array([
+        [0, 2, 4, 5, np.inf, np.inf, np.inf, np.inf],
+        [0, 0, 4, np.inf, np.inf, 7, 5, np.inf],
+        [0, 0, 0, 1, 7, 4, np.inf, np.inf],
+        [0, 0, 0, 0, 10, np.inf, np.inf, np.inf],
+        [0, 0, 0, 0, 0, 1, np.inf, 4],
+        [0, 0, 0, 0, 0, 0, 3, 5],
+        [0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ]))
 
 
 def get_tour(x):
@@ -41,7 +55,8 @@ def get_tour(x):
     node = tour[1]
     while len(tour) < x.shape[0]:
         _, suc = list(
-            (e if e[0] == node else e[::-1]) for e in coords if node in e and 1 == len(set(e).intersection(set(tour))))[
+            (e if e[0] == node else e[::-1]) for e in coords if
+            node in e and 1 == len(set(e).intersection(set(tour))))[
             0]
 
         tour.append(suc)
@@ -52,7 +67,7 @@ def get_tour(x):
 
 def cities_13_glpk(n):
     """13 cities GLPK test"""
-    cost_mat = get_13_cities(n)
+    cost_mat = get_13_cities_tsp(n).cost_mat
     to_mathprog(cost_mat, 'instances/cities_13.mod')
     print('======================================')
     os.system('glpsol --math instances/cities_13.mod')
@@ -60,8 +75,7 @@ def cities_13_glpk(n):
 
 def cities_13_bb(n):
     """13 cities branch and bound test"""
-    cost_mat = get_13_cities(n)
-    instance = TSP(cost_mat)
+    instance = get_13_cities_tsp(n)
 
     print('======================================\nBranch and bound:')
     start = time.time()
@@ -78,8 +92,7 @@ def cities_13_bb(n):
 
 def cities_13_bf(n):
     """13 cities brute force test"""
-    cost_mat = get_13_cities(n)
-    instance = TSP(cost_mat)
+    instance = get_13_cities_tsp(n)
 
     print('======================================\nBrute force:')
     start = time.time()
@@ -93,6 +106,43 @@ def cities_13_bf(n):
     print(f'Time: {elapsed}')
 
     return elapsed
+
+
+def branchbound_13_cities_test():
+    """Executes the test of branch and bound algorithm using the 13 cities instance"""
+    bb = []
+    for n in range(3, 14):
+        print(f'N° CITIES: {n}')
+        bb.append(cities_13_bb(n))
+        print()
+
+    with open('result/bb.json', 'w+') as f:
+        json.dump(bb, f)
+
+
+def bruteforce_13_cities_test():
+    """Executes the test of brute-force algorithm using the 13 cities instance, with at most 11 cities"""
+    bf = []
+    for n in range(3, 12):
+        print(f'N° CITIES: {n}')
+        bf.append(cities_13_bf(n))
+        print()
+
+    with open('result/bf.json', 'w+') as f:
+        json.dump(bf, f)
+
+
+def branchbound_balas_ex1_test():
+    """Executes the test of branch and bound algorithm using the graph from example 1 in Balas, Toth [1983]"""
+    inst = get_balas_ex1_tsp()
+
+    start = time.time()
+    x,z = bb_tsp(inst)
+    end = time.time()
+
+    print(f'Best tour: {get_tour(x)}')
+    print(f'Cost: {z}')
+    print(f'Time: {end - start}')
 
 
 # def big_test():
@@ -109,22 +159,7 @@ def cities_13_bf(n):
 #
 #     print(f'Time: {end - start}')
 
-
 if __name__ == '__main__':
-    bb = []
-    for n in range(3, 14):
-        print(f'N° CITIES: {n}')
-        bb.append(cities_13_bb(n))
-        print()
-
-    with open('result/bb.json', 'w+') as f:
-        json.dump(bb, f)
-
-    bf = []
-    for n in range(3, 12):
-        print(f'N° CITIES: {n}')
-        bf.append(cities_13_bf(n))
-        print()
-
-    with open('result/bf.json', 'w+') as f:
-        json.dump(bf, f)
+    # bruteforce_13_cities_test()
+    # branchbound_13_cities_test()
+    branchbound_balas_ex1_test()
