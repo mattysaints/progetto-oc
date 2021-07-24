645526166bcf897896b3fb307d3f407210780776
Davide Zago <davide.zago@edu.unito.it>
2021-07-24 16:57:20 +0200

    Aggiunto plot di grafi con 0.5 densità

--- efficiency_plot.py
+++ efficiency_plot.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
-with open('result/bf.json', 'r') as f:
+with open('versus/bf.json', 'r') as f:
     bf = json.load(f)
 
-with open('result/bb.json', 'r') as f:
+with open('versus/bb.json', 'r') as f:
     bb = json.load(f)
+
+with open('versus/bb_sparse.json', 'r') as f:
+    bb_sparse = json.load(f)
 
 x = list(range(3, 14))
 
@@ -17,11 +20,14 @@ plt.semilogy(x, bf, 'b', label='brute-force')
 plt.semilogy(x, bb, 'r', label='branch&bound')
 
 plt.title('Branch&Bound vs Brute-Force')
-plt.ylabel('secondi')
+plt.ylabel('log(secondi)')
 plt.xlabel('nodi')
 plt.legend()
 plt.grid()
 
-plt.savefig('result/versus.png', dpi=1200)
+plt.savefig('versus/versus.png', dpi=1200)
 
-plt.savefig()
+plt.semilogy(x, bb_sparse, 'g', label='branch&bound sparse')
+plt.legend()
+
+plt.savefig('versus/versus_sparse.png', dpi=1200)
